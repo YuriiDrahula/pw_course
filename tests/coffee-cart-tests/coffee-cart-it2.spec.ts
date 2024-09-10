eeba5703e37592064197b17910a58032a680633e
YuriiDrahula <ydrahula@gmail.com>
2024-09-10 23:26:22 +0300

    coffe-cart iterration 2

--- tests/coffee-cart-tests/coffee-cart-it2.spec.ts
+++ tests/coffee-cart-tests/coffee-cart-it2.spec.ts
@@ -1,12 +1,15 @@
-import { test, expect, Locator } from "@playwright/test";
+import { test, expect, Locator, Page } from "@playwright/test";
 import {
+  CartPage,
+  CoffeeNames,
+  DataTestCoffeeNames,
   MainCoffeePage,
   PaymentDetailsForm,
   PromoBanner,
-} from "../../coffee-pages/CoffePages";
+} from "./coffee-pages/CoffePagesIt2";
 
-test.beforeEach(({ page }) => {
-  page.goto("https://coffee-cart.app/");
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://coffee-cart.app/");
 });
 
 test.afterEach(({ page }) => {
@@ -17,30 +20,30 @@ test("YD-1: Verify that the total is increased after clicking the coffee card", 
   page,
 }) => {
   const mainCoffeePage = new MainCoffeePage(page);
-  const checkoutItem: Locator = page.locator("ul span");
-  const checkoutCount: Locator = page.locator("ul span[class=unit-desc]");
+  const coffee = new DataTestCoffeeNames();
 
-  // Test
-  await mainCoffeePage.espressoCup.click();
+  await mainCoffeePage.addOneCupToCart(coffee.espresso);
   await expect(mainCoffeePage.totalButton).toContainText("Total: $10.00");
   await mainCoffeePage.totalButton.hover();
-  await expect(checkoutItem.filter({ hasText: "Espresso" })).toBeVisible();
-  await expect(checkoutCount.filter({ hasText: " x 1" })).toBeVisible();
+  await expect(
+    mainCoffeePage.checkoutItem.filter({ hasText: coffee.espresso })
+  ).toBeVisible();
+  await expect(
+    mainCoffeePage.coffeeCount.filter({ hasText: " x 1" })
+  ).toBeVisible();
 });
 
 test('YD-2: Verify that the quantity can be increased in the "Total" pop-up', async ({
   page,
 }) => {
   const mainCoffeePage = new MainCoffeePage(page);
-  const addOneFlatWhiteButton: Locator = page.locator(
-    'button[aria-label="Add one Flat White"]'
-  );
-  const checkoutCount: Locator = page.locator("ul span[class=unit-desc]");
+  const coffeeId = new DataTestCoffeeNames();
+  const coffeeName = new CoffeeNames();
 
-  await mainCoffeePage.flatWhiteCup.click();
+  await mainCoffeePage.addOneCupToCart(coffeeId.flatWhite);
   await mainCoffeePage.totalButton.hover();
-  await addOneFlatWhiteButton.click();
-  await expect(checkoutCount).toContainText(" x 2");
+  await mainCoffeePage.addPlusOneCup(coffeeName.flatWhite);
+  await expect(mainCoffeePage.coffeeCount).toContainText(" x 2");
   await expect(mainCoffeePage.totalButton).toContainText("Total: $36.00");
 });
 
@@ -48,28 +51,18 @@ test('YD-3: Verify that the quantity can be decreased in the "Total" pop-up', as
   page,
 }) => {
   const mainCoffeePage = new MainCoffeePage(page);
-  const removeOneAmericanoButton = page.locator(
-    'button[aria-label="Remove one Americano"]'
-  );
-  const removeOneCafeLatteButton = page.locator(
-    'button[aria-label="Remove one Cafe Latte"]'
-  );
-  const removeOneFlatWhiteButton = page.locator(
-    'button[aria-label="Remove one Flat White"]'
-  );
-  const removeOneMochaButton = page.locator(
-    'button[aria-label="Remove one Mocha"]'
-  );
+  const coffeeId = new DataTestCoffeeNames();
+  const coffeeName = new CoffeeNames();
 
-  await mainCoffeePage.mochaCup.click();
-  await mainCoffeePage.flatWhiteCup.click();
-  await mainCoffeePage.americanoCup.click();
-  await mainCoffeePage.cafeLatteCup.click();
+  await mainCoffeePage.addOneCupToCart(coffeeId.mocha);
+  await mainCoffeePage.addOneCupToCart(coffeeId.flatWhite);
+  await mainCoffeePage.addOneCupToCart(coffeeId.americano);
+  await mainCoffeePage.addOneCupToCart(coffeeId.cafeLatte);
   await mainCoffeePage.totalButton.hover();
-  await removeOneAmericanoButton.click();
-  await removeOneCafeLatteButton.click();
-  await removeOneFlatWhiteButton.click();
-  await removeOneMochaButton.click();
+  await mainCoffeePage.removeOneCup(coffeeName.americano);
+  await mainCoffeePage.removeOneCup(coffeeName.cafeLatte);
+  await mainCoffeePage.removeOneCup(coffeeName.flatWhite);
+  await mainCoffeePage.removeOneCup(coffeeName.mocha);
   await expect(mainCoffeePage.totalButton).toContainText("Total: $0.00");
 });
 
@@ -77,14 +70,13 @@ test("YD-4: Verify that item is added to the cart on the cart page", async ({
   page,
 }) => {
   const mainCoffeePage = new MainCoffeePage(page);
-  const cafeLatteInCartList: Locator = page.locator(
-    '//div[text()="Cafe Latte"]'
-  );
+  const cartPage = new CartPage(page);
+  const coffeeId = new DataTestCoffeeNames();
 
-  await mainCoffeePage.cafeLatteCup.click();
+  await mainCoffeePage.addOneCupToCart(coffeeId.cafeLatte);
   await expect(mainCoffeePage.cartPageLink).toContainText("cart (1)");
   await mainCoffeePage.cartPageLink.click();
-  await expect(cafeLatteInCartList).toBeVisible();
+  await expect(cartPage.getCartItem("Cafe Latte")).toBeVisible();
 });
 
 // Test user credentials
@@ -94,8 +86,9 @@ const userEmail: string = "test@mail.com";
 test("YD-5: Verify payment can be successfully finished", async ({ page }) => {
   const mainCoffeePage = new MainCoffeePage(page);
   const paymentDetailsForm = new PaymentDetailsForm(page);
+  const coffee = new DataTestCoffeeNames();
 
-  await mainCoffeePage.flatWhiteCup.click();
+  await mainCoffeePage.addOneCupToCart(coffee.flatWhite);
   await mainCoffeePage.cartPageLink.click();
   await mainCoffeePage.totalButton.click();
   await paymentDetailsForm.nameInputField.fill(userName);
@@ -113,10 +106,11 @@ test("YD-6: Verify that the promotion offer is displayed after adding 3 items to
 }) => {
   const mainCoffeePage = new MainCoffeePage(page);
   const promoBanner = new PromoBanner(page);
+  const coffee = new DataTestCoffeeNames();
 
-  await mainCoffeePage.flatWhiteCup.click();
-  await mainCoffeePage.espressoConPannaCup.click();
-  await mainCoffeePage.mochaCup.click();
+  await mainCoffeePage.addOneCupToCart(coffee.cafeBreve);
+  await mainCoffeePage.addOneCupToCart(coffee.espressoConPanna);
+  await mainCoffeePage.addOneCupToCart(coffee.espressoMacchiato);
   await expect(promoBanner.bannerDiv).toBeVisible();
   await expect(promoBanner.bannerTitle).toContainText(
     "It's your lucky day! Get an extra cup of Mocha for $4."
@@ -128,41 +122,35 @@ test("YD-7: Verify that promotion item is added to the cart with the promotion p
 }) => {
   const mainCoffeePage = new MainCoffeePage(page);
   const promoBanner = new PromoBanner(page);
-  const discountedMocha: Locator = page.locator(
-    '//div[text()="(Discounted) Mocha"]'
-  );
-  const discountedMochaPrice = page.locator(
-    '//div[text()="(Discounted) Mocha"]/following-sibling::div[2]'
-  );
+  const cartPage = new CartPage(page);
+  const coffee = new DataTestCoffeeNames();
 
-  await mainCoffeePage.espressoCup.click();
-  await mainCoffeePage.americanoCup.click();
-  await mainCoffeePage.cafeLatteCup.click();
+  await mainCoffeePage.addOneCupToCart(coffee.espresso);
+  await mainCoffeePage.addOneCupToCart(coffee.americano);
+  await mainCoffeePage.addOneCupToCart(coffee.cafeLatte);
   await promoBanner.yesButton.click();
   await mainCoffeePage.cartPageLink.click();
-  await expect(discountedMocha).toBeVisible();
-  await expect(discountedMochaPrice).toContainText("$4.00");
+  await expect(cartPage.discountedMocha).toBeVisible();
+  await expect(cartPage.discountedMochaPrice).toContainText("$4.00");
 });
 
 test("YD-8: Verify that the cart is empty after removing all items", async ({
   page,
 }) => {
   const mainCoffeePage = new MainCoffeePage(page);
-  const itemList: Locator = page.locator('[class="list-header"]');
-  const deleteItemButton: Locator = page
-    .locator("li[class=list-item] button[class=delete]")
-    .first();
+  const cartPage = new CartPage(page);
+  const coffee = new DataTestCoffeeNames();
 
-  await mainCoffeePage.espressoConPannaCup.click();
-  await mainCoffeePage.espressoMacchiatoCup.click();
-  await mainCoffeePage.cafeBreveCup.click();
+  await mainCoffeePage.addOneCupToCart(coffee.cafeBreve);
+  await mainCoffeePage.addOneCupToCart(coffee.espressoMacchiato);
+  await mainCoffeePage.addOneCupToCart(coffee.cappuccino);
   await mainCoffeePage.cartPageLink.click();
 
-  while (await itemList.isVisible()) {
-    await deleteItemButton.click();
+  while (await cartPage.itemList.isVisible()) {
+    await cartPage.deleteItemButton.click();
   }
 
-  await expect(page.getByRole("paragraph")).toContainText(
+  await expect(cartPage.emptyStateText).toContainText(
     "No coffee, go add some."
   );
 });
